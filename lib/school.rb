d6453560ca5bde9f12f57f11b68a9ef302ace73c
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-22 16:24:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,8 +13,8 @@ class School
   end
 
   def add_student(student_name, grade)
-    roster[grade] = []
-    roster[grade] << student_name
+    self.roster[grade] = []
+    self.roster[grade] << student_name
   end
 
 
