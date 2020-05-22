d3bfa06008815195a2ee94e9639a965c2710d0f1
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-22 16:08:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,9 +3,10 @@ class School
   attr_accessor
   attr_reader :school_name
 
+  ROSTER = []
+
   def initialize(name)
     @school_name = name
-    ROSTER = []
   end
 
   def add_student(student_name, grade)
